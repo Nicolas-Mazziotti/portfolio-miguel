e89b90421dd4d908627484c7e7e2c937ec54694e
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2025-01-31 12:22:35 -0300

    feat footer: create footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './Components/About/About'
 import Skills from './Components/Skills/Skills'
 import Portfolio from './Components/Portfolio/Portfolio'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
     <Skills/>
     <Portfolio/>
     <Contact/>
+    <Footer/>
     </>
   )
 }
